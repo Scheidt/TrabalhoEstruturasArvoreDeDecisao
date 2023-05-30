0ddf9aca0e2373f085fd199ed6a81fe56b809b73
Pedro Scheidt <pedrohenriqueprazeres@hotmail.com>
2023-05-30 15:12:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,75 @@ global raiz
 raiz = None
 pontuacao = 0
 
+class Play(Folha, Nodo):
+    def __init__(self) -> None:
+        self.__raiz = None
+        self.__pontuacao = 0
+        if False:
+            self.preDef
+
+    def pontuacao(self):
+        # Mostrar pontuação
+        pass
+
+    def jogar(self):
+        # Percorrer lista e se não der certo, adicionar
+        pass
+
+    def sanitizarEntrada(self, entrada: str, possibilidades, mensagem):
+        while not entrada in possibilidades:
+            entrada = input(mensagem)
+        return entrada
+
+    def adicionar(self, pai: Nodo, caminhoPai: str):
+
+        if self.__raiz is None:
+            valCarta = input("Qual é a primeira carta a ser adicionada? ")
+            self.__raiz = Folha(valCarta)
+        elif isinstance(self.__raiz, Folha):
+            resposta = input(f"Sua carta é {self.__raiz} ['s'/'n']? ")
+            resposta = self.sanitizarEntrada(resposta, ('s', 'n'), "Sua resposta pode ser somente 's' ou 'n', por favor, tente novamente: ")
+        else: 
+            # Pegar valor para o novo filho, criar um nodo com o filho anterior e o novo
+            # filho e colocar como filho do pai na posição caminhoPai
+            filhoNovo = input("Qual era a sua carta? ")
+            if caminhoPai == 's':
+                filhoOriginal = pai.sim
+                caminhoPai = 'sim'
+            else:
+                filhoOriginal = pai.nao
+                caminhoPai = 'nao'
+            pergunta = input(f"Insira uma pergunta de sim ou não que diferencie {filhoOriginal} de {filhoNovo}: ")
+            posFilhoOriginal = input(f"E {filhoNovo} {pergunta} ['s'/'n'] ")
+            posFilhoOriginal = self.sanitizarEntrada(posFilhoOriginal, ('s', 'n'), "Sua resposta pode ser somente 's' ou 'n', por favor, tente novamente: ")
+            # Adicionar o filho em si
+            
+        pass
+
+    def adicionarDev(self, caminho: list, posFilhoVelho: str, valFilhoNovo: str, pergunta: str):
+        if self.__raiz is None: # Se não houver nenhuma carta inscrita
+            self.__raiz = Folha(valFilhoNovo)
+        elif isinstance(self.__raiz, Folha): # Se houver somente uma carta inscrita
+            if posFilhoVelho == 'sim':
+                self.__raiz = Nodo(pergunta, self.__raiz, Folha(valFilhoNovo))
+            elif posFilhoVelho == 'nao':
+                self.__raiz = Nodo(pergunta, Folha(valFilhoNovo), self.__raiz)
+        else:
+            nodo = self.__raiz
+            for passo in caminho[0, -2]: # Percorre a árvore com o caminho dado até o pai do futuro nodo
+                nodo = nodo.passo
+            filhoVelho = nodo.caminho[-1] # Pega a folha que será adicionada no novo nodo
+            if posFilhoVelho == 'sim':
+                nodo.caminho[-1] = Nodo(pergunta, filhoVelho, Folha(valFilhoNovo))
+            elif posFilhoVelho == 'nao':
+                nodo.caminho[-1] = Nodo(pergunta, Folha(valFilhoNovo), filhoVelho)
+            
+
+    def preDef(self):
+        # Criar uma pre-definição de arvore para mostrar melhor. Utilizar adicionarDev para colocar em posições corretas
+        pass
+
+# Código legado.
 def jogar():
     atual = raiz
     anterior = None
